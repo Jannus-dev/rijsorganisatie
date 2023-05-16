03a4fa114a55bf3c0d65f295e154f0afe931935e
Jannus <jan.honing2@gmail.com>
2023-05-16 09:38:13 +0200

    Start Header

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <a href="">
+            <img src="" alt="Logo">
+        </a>
+        <a href="">Bestemmingen</a>
+        <a href="">Vakantie</a>
+        <a href="">Willekeurig</a>
+        <a href="">Deals</a>
+        <a href="">Ons Bedrijf</a>
+        <a href="">Login</a>
+    </nav>
+</header>
