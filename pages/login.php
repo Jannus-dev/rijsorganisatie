e452bfd0587b372fe16690bbfde6945b2123564e
Maik <maikew2006@gmail.com>
2023-05-24 14:40:19 +0200

    kleine update

--- pages/login.php
+++ pages/login.php
@@ -5,30 +5,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vakantie</title>
+    <title>Login</title>
     <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Laila:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div class="background">
         <header>
-            <nav>
+            <div class="logo">
                 <a href="../index.php">
-                    <img src="../img reisbureau\main-logo.jpg" alt="Logo">
+                    <img src="../img-reisbureau\main-logo.jpg" alt="Logo">
                 </a>
-                <a href="bestemmingen.php">Bestemmingen</a>
-                <a href="vakantie.php">Vakantie</a>
-                <a href="willekeurig.php">Willekeurig</a>
-                <a href="deals.php">Deals</a>
-                <div class="box">
-                    <img src="../img reisbureau\linkervleugel.png" alt="">
-                    <a href="login.php">Login</a>
-                    <img src="../img reisbureau\rechtervleugel.png" alt="">
-                </div>
-            </nav>
+            </div>
+            <ul>
+                <li><a href="bestemmingen.php">Bestemmingen</a></li>
+                <li><a href="vakantie.php">Vakantie</a></li>
+                <li><a href="willekeurig.php">Willekeurig</a></li>
+                <li><a href="deals.php">Deals</a></li>
+                <li>
+                    <div class="box">
+                        <img src="../img-reisbureau\linkervleugel.png" alt="">
+                        <a href="login.php">Login</a>
+                        <img src="../img-reisbureau\rechtervleugel.png" alt="">
+                    </div>
+                </li>
+            </ul>
         </header>
+        <nav>
+            <div class="container">
+                <p>Voorgestelde vakanties</p>
+                <div class="filter">
+                    <img src="../img-reisbureau/Filter-Icon.png" alt="">
+                </div>
+            </div>
+        </nav>
     </div>
-    <footer> <a href="Ons-Bedrijf.php">Ons Bedrijf</a></footer>
+
 </body>
 
 </html>
