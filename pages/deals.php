db60903f3da9931faee6260ef6393297184c0bc8
Maik Hendriks <maikew2006@gmail.com>
2023-06-17 21:28:06 +0200

    contact + onsbedrijf pagina af

--- pages/deals.php
+++ pages/deals.php
@@ -8,6 +8,8 @@
     <title>Deals</title>
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/deals.css">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Laila:wght@300&display=swap" rel="stylesheet">
@@ -34,8 +36,67 @@
                 <li><a href="login.php">Login</a></li>
             </ul>
         </header>
-        
+        <div class="container">
+            <div class="nav">
+                <div class="select-region-box">
+                    <div class="select-region">
+                        <div class="container">
+                            <h2>Select a Region</h2>
+                            <form>
+                                <label>
+                                    <input type="radio" name="region" value="Europe">
+                                    Europe
+                                </label>
+                                <label>
+                                    <input type="radio" name="region" value="North America">
+                                    North America
+                                </label>
+                                <label>
+                                    <input type="radio" name="region" value="South America">
+                                    South America
+                                </label>
+                                <label>
+                                    <input type="radio" name="region" value="Asia">
+                                    Asia
+                                </label>
+                                <label>
+                                    <input type="radio" name="region" value="Africa">
+                                    Africa
+                                </label>
+                                <label>
+                                    <input type="radio" name="region" value="Australia">
+                                    Australia
+                                </label>
+                            </form>
+                            <button onclick="filterRegion()">Filter</button>
+                        </div>
+
+                        <script>
+                            function filterRegion() {
+                                var regionRadios = document.getElementsByName("region");
+                                var selectedRegion = "";
+
+                                for (var i = 0; i < regionRadios.length; i++) {
+                                    if (regionRadios[i].checked) {
+                                        selectedRegion = regionRadios[i].value;
+                                        break;
+                                    }
+                                }
+
+                                if (selectedRegion !== "") {
+                                    console.log("Geselecteerde regio: " + selectedRegion);
+                                } else {
+                                    alert("Kies een regio A.U.B.");
+                                }
+                            }
+                        </script>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+
 
     <?php include_once("../footer.php"); ?>
 </body>
