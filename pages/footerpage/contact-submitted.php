868ee4ac0c1d508af6547112bda48fd506847782
Maik Hendriks <maikew2006@gmail.com>
2023-06-18 23:58:55 +0200

    small fix

---
+++ pages/footerpage/contact-submitted.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../css/header.css">
+    <link rel="stylesheet" href="../../css/footer.css">
+    <link rel="stylesheet" href="../../css/onsbedrijf.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Laila:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="background">
+        <header>
+            <div class="logo">
+                <a href="../../index.php">
+                    <img src="../../img-reisbureau\main-logo.jpg" alt="Logo">
+                </a>
+            </div>
+            <ul>
+                <li><a href="../bestemmingen.php">Bestemmingen</a></li>
+                <li><a href="../willekeurig.php">Willekeurig</a></li>
+                <li><a href="../deals.php">Deals</a></li>
+                <li> <a href="../login.php">Login</a></li>
+            </ul>
+        </header>
+        <div class="textbox-container">
+            <div class="textbox">
+                <h1>Bedankt voor het versturen van je bericht!</h1>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>Rights all owned by GeenIdee.nl</p>
+        <p>Privacy and service agreement</p>
+        <p><a href="contact.php">Contact</a></p>
+        <p><a href="onsbedrijf.php">Ons Bedrijf</a></p>
+    </footer>
+</body>
+
+</html>
