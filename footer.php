69c645c10a89e5f820fb93f73018f605b3bcaa9a
Maik Hendriks <maikew2006@gmail.com>
2023-06-19 20:13:39 +0200

    push

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
-    <p>Rights all owned by GeenIdee.nl</p>
-    <p>Privacy and service agreement</p>
+    <p>Algemene voorwaarden</p>
+    <p>Privacy policy</p>
     <p><a href="footerpage/contact.php">Contact</a></p>
     <p><a href="footerpage/onsbedrijf.php">Ons Bedrijf</a></p>
 </footer>
---
+++ pages/dropertje.js
@@ -0,0 +1,21 @@
+function searchFunction() {
+    var input = document.getElementById("search-bar").value;
+    if (input.length === 0) {
+        document.getElementById("dropdown").innerHTML = "";
+        return;
+    }
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var response = JSON.parse(this.responseText);
+            var dropdownContent = "";
+            for (var i = 0; i < response.length; i++) {
+                dropdownContent += "<a href='#'>" + response[i] + "</a>";
+            }
+            document.getElementById("dropdown").innerHTML = dropdownContent;
+        }
+    };
+    xmlhttp.open("GET", "landsearge.php?input=" + input, true);
+    xmlhttp.send();
+}
